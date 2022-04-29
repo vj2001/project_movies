197d9a68fdac3134df20d52c0bd84096c6f23b3e
Vardhman jain <manu1242000@gmail.com>
2022-04-29 13:32:20 +0530

    renamed a file favou -> favo

--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Favorite } = require('../models/Favourite');
+const { Favorite } = require('../models/Favorite');
 
 router.post('/favoriteNumber', (req, res) => {
 
