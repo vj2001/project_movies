2e06d356c0010d478f6621dac68c87c94466be1d
vj2001 <vishaljain63957@gmail.com>
2022-05-01 21:51:06 +0530

    production build added and deployed to heroku

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,5 @@
 module.exports = {
     // mongoURI:process.env.MONGO_URI
-    mongoURI:"mongodb+srv://vishal:vishal123@cluster0.ed73q.mongodb.net/movies?retryWrites=true&w=majority"
+     mongoURI:"your mongodb url"
+
 }
