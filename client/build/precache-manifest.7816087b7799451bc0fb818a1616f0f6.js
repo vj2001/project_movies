73b57c6bc28ffdbc8c1c0e300e4b1532ecb15f39
vj2001 <vishaljain63957@gmail.com>
2022-05-01 21:23:39 +0530

    app deployed

---
+++ client/build/precache-manifest.7816087b7799451bc0fb818a1616f0f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5314b8e5c6f1939fa3c731cb360927ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2cd3e9ef2940716d46e",
+    "url": "/static/css/2.01822209.chunk.css"
+  },
+  {
+    "revision": "1eea1d3f71607267e5d4",
+    "url": "/static/css/main.659850de.chunk.css"
+  },
+  {
+    "revision": "d2cd3e9ef2940716d46e",
+    "url": "/static/js/2.c37c78e9.chunk.js"
+  },
+  {
+    "revision": "1eea1d3f71607267e5d4",
+    "url": "/static/js/main.548fb4d2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
