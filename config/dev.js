33bc2981e61ab791bd09acea61c2f949afc3a961
vj2001 <vishaljain63957@gmail.com>
2022-05-01 22:04:36 +0530

    updates

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-    // mongoURI:"mongodb://localhost:27017/movies"
-    mongoURI:"mongodb+srv://vishal:vishal123@cluster0.ed73q.mongodb.net/movies?retryWrites=true&w=majority"
+    // mongoURI:process.env.MONGO_URI
+    mongoURI:"your mongodb url"
 }
